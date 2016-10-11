14e98f95312478a40e96a9cc08fbe7b9b122f9d1
juyoung0 <ojmme625@gmail.com>
2016-10-11 14:21:45 +0900

    first commit

---
+++ dream/config/routes.rb
@@ -0,0 +1,7 @@
+Rails.application.routes.draw do
+  get 'second/index'
+
+  get 'first/index'
+  get 'first/index2'
+  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+end
