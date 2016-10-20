fd79a4634e646330ca3b52557bcdcceca3ec6b24
juyoung0 <ojmme625@gmail.com>
2016-10-20 12:22:13 +0900

    solve csv problem

--- dream/app/assets/javascripts/application.js
+++ dream/app/assets/javascripts/application.js
@@ -14,3 +14,5 @@
 //= require jquery_ujs
 //= require turbolinks
 //= require_tree .
+//= require d3
+//= require d3.tip
--- dream/config/routes.rb
+++ dream/config/routes.rb
@@ -1,6 +1,5 @@
 Rails.application.routes.draw do
   get 'second/index'
-
   get 'first/index'
   get 'first/index2'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
